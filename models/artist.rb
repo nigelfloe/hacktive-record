ad46714655c366c3a8575bafd219d3d0a186394b
nigelfloe <dwr229@gmail.com>
2015-08-18 11:17:50 -0400

    works

--- models/artist.rb
+++ models/artist.rb
@@ -17,4 +17,38 @@ class Artist
     results = DB.execute(sql, id)
     results.map {|row| self.new_from_row(row)}.first
   end
+
+  def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM artists
+    SQL
+    results = DB.execute(sql)
+    results.map {|row| self.new_from_row(row)}
+  end
+
+  def persisted?
+    !!id
+  end
+
+  def save
+    persisted? ? update : insert
+  end
+
+  private
+  def insert
+    sql = <<-SQL
+      INSERT INTO artists (name) VALUES (?)
+    SQL
+    DB.execute(sql, name)
+  end
+
+  def update
+    sql = <<-SQL
+      UPDATE artists
+      SET name=?
+      WHERE id=?
+    SQL
+    DB.execute(sql, name, id)
+  end
 end
